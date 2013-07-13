ec2f0767fcb3435d55c33375a0093664cce4a0bf
Nidia Benitez <nidbep@hotmail.com>
2013-07-13 15:49:59 -0500

    clase6
    clase6

---
+++ js/API/eventos.js
@@ -0,0 +1,51 @@
+//Eventos
+$(document).ready(function(e){
+	document.addEventListener("deviceready", function(){
+		if(!isLogin())
+			window.location.href = '#login';
+	   $('#regSend').tap(function(){
+		  if($('#regNom').val()!='' && $('#regTel').val()!='' && $('#regMail').val()!='' && $('#regFoto').attr('rel')!=undefined){
+			  var nom = $('#regNom').val();
+			  var tel = $('#regTel').val();
+			  var mail = $('#regMail').val();
+			  var foto = $('#regFoto').attr('rel');
+			  enviarDatos(nom, tel, mail, foto);
+			  //navigator.notification.alert(nom +'\n'+ tel +'\n'+ mail, null, "Hotel","Aceptar");
+		  }else{
+			  navigator.notification.alert('Todos los campos son requeridos', null, "Hotel","Ok");
+		  }
+	   });
+	   $('#regFoto').tap(function(){
+		   tomarFoto();
+	   });
+	   //---------------Reservaciones---------------//
+	   var nr1 = $('#nr1');
+	   nr1.find('ul[data-role=listview] li').tap(function(){
+		   if($(this).index()!=0){
+				nr1.attr('th',$(this).index());
+				$(this).css('background','#0ff000');
+		   }
+	   });
+	   $('#sh').tap(function(){
+		   if(nr1.attr('th')!=undefined && nr1.attr('th')!=''){
+			   window.location.href = '#nr2';
+		   }
+	   });
+	   $('#rh').tap(function(){
+		   if(isConnected())
+		   		subirReserva(0,nr1.attr('th'), $('#rHabitaciones').val(), $('#rDias').val(), $('#rPersonas').val());
+			else
+				guardarReserva(nr1.attr('th'), $('#rHabitaciones').val(), $('#rDias').val(), $('#rPersonas').val());
+	   });
+		$('a[href=#historial]').tap(function(event){
+			leerHistorial();
+		});
+	}, false);
+});
+
+function isLogin(){
+	if(window.localStorage.getItem('id')!=undefined)
+		return true;
+	else
+		return false;
+}
